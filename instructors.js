3783f87fb5f14e1c0f7638fc70982f2c575edb6a
Evandro Gibicoski <evandrogibicoski@gmail.com>
2020-05-01 17:14:24 -0300

    updated

--- instructors.js
+++ instructors.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const data = require('./data.json');
 
-exports.show = function(req, res) {
+exports.show = function(req, res) {fix 
     const { id } = req.params;
 
     const foundInstructor = data.instructors.find(function(instructor) {
