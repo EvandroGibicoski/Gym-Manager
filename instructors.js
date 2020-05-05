4b8b897312990da3f6d2992e12c050ce6c9cce73
EvandroGibicoski <evandrogibicoski@outlook.com>
2020-05-05 10:07:54 -0300

    fix: birth and services

--- instructors.js
+++ instructors.js
@@ -1,35 +1,22 @@
 const fs = require('fs');
 const data = require('./data.json');
+const { age } = require('./utils')
 
-exports.show = function(req, res) {fix 
+exports.show = function(req, res) {
     const { id } = req.params;
-
+    
     const foundInstructor = data.instructors.find(function(instructor) {
        return instructor.id == id;
     });
         if(!foundInstructor)
             return res.send("Instructor not found!")
 
-        function age(timestamp) {
-            const today = new Date()
-            const birthDate = new Date(timestamp)
-
-        let age = today.getFullYear() - birthDate.getFullYear()
-
-        const month = today.getMonth() - birthDate.getMonth()
-            if(month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
-                age = age -1
-            }
-
-            return age
-
-        }
         const instructor = {
             ...foundInstructor,
             age: age(foundInstructor.birth),
-            gender: "",
+            gender: foundInstructor.gender,
             services: foundInstructor.services.split(","),
-            created_at: ""
+            created_at: new Intl.DateTimeFormat("pt-br").format(foundInstructor.created_at)
 
         }
             return res.render("instructors/show", { instructor })
@@ -45,7 +32,7 @@ exports.post = function(req, res) {
         }
     }
     
-    let { avatar_url, name, birth, gender, service} = req.body;
+    let { avatar_url, name, birth, gender, services} = req.body;
 
     const created_at = Date.now();
     const id = Number(data.instructors.length +1);
