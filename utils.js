49b6101ca710ec92cc19ccbb39e0f82002a2f02e
EvandroGibicoski <evandrogibicoski@outlook.com>
2020-05-09 17:25:15 -0300

    add news

--- utils.js
+++ utils.js
@@ -8,21 +8,17 @@ module.exports = {
     const month = today.getMonth() - birthDate.getMonth()
         if(month < 0 || month == 0 && today.getDate() <= birthDate.getDate()) {
             age = age - 1
-        }
+       }
 
         return age
 
     },
     date: function(timestamp) {
-        // get date
         const date = new Date(timestamp)
-        // get year
-        const year = date.getFullYear()
-        // get month 
-        const month = date.getFullYear() + 1
-        // get day
-        const day = date.getFullYear()
+        const year = date.getUTCFullYear()
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        const day = `0${date.getUTCDate()}`.slice(-2)
 
-        console.log(`${year}-${month}-${day}`)
+        return `${year}-${month}-${day}`
     }
 }
